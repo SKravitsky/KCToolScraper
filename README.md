6dbd38e8cea7c1c5db61bd653c80af2f6d2e5e88
SKravitsky <steven.kravitsky@gmail.com>
2022-01-13 00:52:59 +0000

    Adding mailing function to the application and updating readme

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Tool of the Day Scraper
 
 Learning GO by making a simple web scraper
+
+## Setup
+
+You'll need to setup three environment variables
+
+GPW for the password
+
+MAILFROM for the "from"
+
+MAILTO for the "to"
--- main.go
+++ main.go
@@ -1,26 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"log"
-
-	// "net/http"
+	"net/smtp"
+	"os"
 
 	"github.com/gocolly/colly/v2"
 )
 
-// func ping(w http.ResponseWriter, r *http.Request) {
-// 	log.Println("Ping")
-// 	w.Write([]byte("ping"))
-// }
+type toolLink struct {
+	desc string
+	link string
+}
 
-// func getData(w http.ResponseWriter, r *http.Request) {
-// 	URL := "https://www.kctool.com/tool-of-the-day/"
-// 	c := colly.NewCollector(colly.MaxDepth(1))
+type smtpServer struct {
+	host string
+	port string
+}
 
-// }
+func (s *smtpServer) Address() string {
+	return s.host + ":" + s.port
+}
 
-func scrape() {
+func scrape() toolLink {
 	log.Println("Starting Scraping")
+	var tools toolLink
+
 	c := colly.NewCollector(
 		colly.AllowedDomains("kctool.com", "www.kctool.com"),
 		colly.MaxDepth(1),
@@ -28,8 +34,8 @@ func scrape() {
 
 	c.OnHTML(".card-figure a[href]", func(e *colly.HTMLElement) {
 		if e.Attr("class") == "card-figure__link" {
-			log.Println("Got the description: ", e.Attr("aria-label"))
-			log.Println("Link to tool: ", e.Attr("href"))
+			tools.desc = e.Attr("aria-label")
+			tools.link = e.Attr("href")
 		}
 	})
 
@@ -38,40 +44,29 @@ func scrape() {
 	})
 
 	c.Visit("https://www.kctool.com/tool-of-the-day")
-
+	return tools
 }
 
-// func crawl() {
-// 	log.Println("Testing Crawler")
-// 	c := colly.NewCollector(
-// 		colly.AllowedDomains("factretriever.com", "www.factretriever.com"),
-// 	)
+func mail(t *toolLink) {
+	from := os.Getenv("MAILFROM")
+	password := os.Getenv("GPW")
+	to := []string{os.Getenv("MAILTO")}
 
-// 	c.OnHTML(".factsList li", func(e *colly.HTMLElement) {
-// 		factId, err := strconv.Atoi(e.Attr("id"))
-// 		if err != nil {
-// 			log.Println("Could not get ID")
-// 		}
-// 		log.Println("Got the FactID of ", factId)
+	smtpServer := smtpServer{host: "smtp.gmail.com", port: "587"}
+	message := []byte(fmt.Sprintf("%q", t))
 
-// 		factDesc := e.Text
-// 		log.Println("Got the description: ", factDesc)
-// 	})
+	auth := smtp.PlainAuth("", from, password, smtpServer.host)
+	err := smtp.SendMail(smtpServer.Address(), auth, from, to, message)
 
-// 	c.OnRequest(func(r *colly.Request) {
-// 		log.Println("Visiting ", r.URL.String())
-// 	})
+	if err != nil {
+		log.Println(err)
+		return
+	}
 
-// 	c.Visit("https://www.factretriever.com/rhino-facts")
-
-// }
+	log.Println("Email Sent!")
+}
 
 func main() {
-	scrape()
-	// crawl()
-	// port := ":8888"
-	// http.HandleFunc("/ping", ping)
-
-	// log.Println("Listening on port", port)
-	// log.Fatal(http.ListenAndServe(port, nil))
+	tools := scrape()
+	mail(&tools)
 }
